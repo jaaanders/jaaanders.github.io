5cfb032771508d71e1aa60f9a15ce07a61a18252
jaaanders <jordananderson909@gmail.com>
2016-12-15 01:15:13 -0800

    linking .css to .html

--- sketch.js
+++ sketch.js
@@ -1,34 +1,56 @@
-﻿
-var angle = 0;
-var slider;
+// Coding Rainbow
+// Daniel Shiffman
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/fcdNSZ9IzJM
+
+var tree = [];
+var leaves = [];
+
+var count = 0;
 
 function setup() {
-    createCanvas(400, 400);
-    slider = createSlider(0, TWO_PI, PI / 4, 0.01);
+  createCanvas(400, 400);
+  var a = createVector(width / 2, height);
+  var b = createVector(width / 2, height - 100);
+  var root = new Branch(a, b);
+
+  tree[0] = root;
+}
+
+function mousePressed() {
+  for (var i = tree.length - 1; i >= 0; i--) {
+    if (!tree[i].finished) {
+      tree.push(tree[i].branchA());
+      tree.push(tree[i].branchB());
+    }
+    tree[i].finished = true;
+  }
+  count++;
+
+  if (count === 6) {
+    for (var i = 0; i < tree.length; i++) {
+      if (!tree[i].finished) {
+        var leaf = tree[i].end.copy();
+        leaves.push(leaf);
+      }
+    }
+  }
+
 }
 
 function draw() {
-    background(51);
-    angle = slider.value();
-    stroke(255);
-    translate(200, height);
-    branch(100);
+  background(51);
 
-}
+  for (var i = 0; i < tree.length; i++) {
+    tree[i].show();
+    //tree[i].jitter();
+  }
 
-function branch(len) {
-    line(0, 0, 0, -len);
-    translate(0, -len);
-    if (len > 4) {
-        push();
-        rotate(angle);
-        branch(len * 0.67);
-        pop();
-        push();
-        rotate(-angle);
-        branch(len * 0.67);
-        pop();
-    }
+  for (var i = 0; i < leaves.length; i++) {
+    fill(255, 0, 100, 100);
+    noStroke();
+    ellipse(leaves[i].x, leaves[i].y, 8, 8);
+    leaves[i].y += random(0, 2);
+  }
 
-    //line(0, 0, 0, -len * 0.67);
 }
