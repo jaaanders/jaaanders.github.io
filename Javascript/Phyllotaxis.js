09b8f2110d04b2cadf6b302527e8173a5f195ae9
jaaanders <jordananderson909@gmail.com>
2017-01-15 15:11:05 -0800

    fixed Phyllotaxis.js

--- Javascript/Phyllotaxis.js
+++ Javascript/Phyllotaxis.js
@@ -1,16 +1,23 @@
-﻿var n = 0;
-var c = 75;
+﻿// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/KWoJgHFYWxY
+
+var n = 0;
+var c = 3;
+
+var points = [];
 
 var start = 0;
 
 function setup() {
     createCanvas(400, 400);
-    background(0);
     angleMode(DEGREES);
-    //colorMode(HSB);
+    colorMode(HSB);
 }
 
 function draw() {
+    background(0);
     translate(width / 2, height / 2);
     rotate(n * 0.3);
     for (var i = 0; i < n; i++) {
@@ -18,12 +25,12 @@ function draw() {
         var r = c * sqrt(i);
         var x = r * cos(a);
         var y = r * sin(a);
-        //var hu = cos(start + i * 0.5);
-        //hu = map(hu, -1, 1, 0, 360);
-        fill(200, 100);
+        var hu = sin(start + i * 0.5);
+        hu = map(hu, -1, 1, 0, 360);
+        fill(hu, 255, 255);
         noStroke();
-        ellipse(x, y, 8, 8);
+        ellipse(x, y, 4, 4);
     }
     n += 5;
-    start += 1;
+    start += 5;
 }
